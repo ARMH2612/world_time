5b47a0b998ec837449400dd46df08a260d2dac4e
Houssame Rahiche <houssamerahiche@gmail.com>
2021-05-15 01:53:17 +0200

    app starter : first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spotify_ui/pages/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Home());
+  }
+}
