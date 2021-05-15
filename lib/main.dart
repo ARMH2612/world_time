2027b85fc11aa0d557455cb634842ccd066227c0
Houssame Rahiche <houssamerahiche@gmail.com>
2021-05-15 02:23:24 +0200

    second commit: add routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spotify_ui/pages/chose_location.dart';
 import 'package:flutter_spotify_ui/pages/home.dart';
+import 'package:flutter_spotify_ui/pages/loading.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,6 +10,13 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Home());
+    return MaterialApp(
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location': (context) => ChooseLocation(),
+      },
+    );
   }
 }
